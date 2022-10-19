8b4322ff0303747a203f5f8191495cccb1b6d769
Brendan Thompson <Brendan Thompson@LAPTOP-RT4T4OF2>
2022-10-18 20:13:38 -0700

    Add project files.

---
+++ 586 Proj/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _586_Proj;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 586 Proj/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace _586_Proj;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
